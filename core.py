650db28aa1526512c9cdcd81f73e4b5e01c1b0f0
dalor <dalor@i.ua>
2019-05-24 04:03:07 +0300

    Init

---
+++ core.py
@@ -0,0 +1,68 @@
+from dtelbot.oop import Bot
+from dtelbot.inline_keyboard import button, markup
+from dtelbot.inline import article
+
+from dsr_checker import DSRChecker
+
+import os
+
+BOT_ID = os.environ['BOT_ID']
+
+def stop_checker(a):
+    checker = a.session.get('checker')
+    if checker:
+        if checker.is_run:
+            checker.stop()
+            a.msg('Message handler was stopped').send()
+
+chats = {}
+
+class Core:
+    b = Bot(BOT_ID)
+    message = b.message
+    callback = b.callback_query
+
+    @message('/start')
+    def start(self, a):
+        a.msg('Send me url of chat').send()
+
+    @message('(https?:\/\/[^\/]+).*\/chat\/([^\/]+)')
+    def joinChat(self, a):
+        #stop_checker(a)
+        checker = chats.get(a.args[2])
+        if not checker:
+            checker = DSRChecker(a.bot, a.chat_id, a.args[1] + '/api/chat/' + a.args[2])
+            if checker.token:
+                chats[a.args[2]] = checker
+                checker.start()
+            else:
+                a.msg('You can`t connect this chat').send()
+                return
+        else:
+            checker.chat_ids.append(a.chat_id)
+        a.session['checker'] = checker
+        a.msg('Connected to chat \'{}\''.format(a.args[2])).send()
+            
+
+    @message('/stop')
+    def stop(self, a):
+        stop_checker(a)
+
+    @message('(.+)')
+    def send(self, a):
+        checker = a.session.get('checker')
+        if checker:
+            try:
+                checker.sendMessage(a.args[1], a.chat_id, a.data['message_id'])
+            except:
+                stop_checker(a)
+
+c = Core()
+
+@app.route('/{}'.format(BOT_ID), methods=['POST']) #Telegram should be connected to this hook
+def webhook():
+    c.b.check(request.get_json())
+    return 'ok', 200
+
+if __name__ == '__main__':
+app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
---
+++ dsr_checker.py
@@ -0,0 +1,59 @@
+from threading import Thread
+import requests
+import time
+
+class DSRChecker(Thread):
+    def __init__(self, bot, chat_id, chat_url):
+        Thread.__init__(self)
+        self.bot = bot
+        self.chat_ids = [chat_id]
+        self.chat_url = chat_url[:-2] if chat_url[-1] == '/' else chat_url
+        self.s = requests.Session()
+        self.token = None
+        self.last_id = 0
+        self.count = 5000
+        self.is_run = True
+        self.sleep_time = 0.3
+        self.my_messages = []
+        self.auth()
+
+    def auth(self):
+        res = self.s.get(self.chat_url + '/joinUser').json()
+        if res['ok']:
+            self.token = res['result'][0]['content']
+
+    def run(self):
+        while self.is_run:
+            try:
+                messages = self.s.get(self.chat_url + '/getAll', params={'from': self.last_id + 1, 'to': self.last_id + self.count}).json()
+                for mess in messages:
+                    if mess['id'] > self.last_id:
+                        #if not mess['content'] in self.my_messages:
+                            self.bot.more([self.bot.msg(mess['content'], chat_id=chat_id) for chat_id in self.chat_ids])
+                        #else:
+                            #self.my_messages.remove(mess['content'])
+                if messages:
+                    self.last_id = messages[-1]['id']
+            except:
+                pass
+            time.sleep(self.sleep_time)
+
+    def sendMessage(self, text, chat_id, message_id):
+        result  = self.s.get(self.chat_url + '/sendmessage', params={'content': text}).json()
+        print(result)
+        if result['ok']:
+            self.my_messages.append(text)
+            res = self.bot.msg('Sended', chat_id=chat_id, reply_to_message_id=message_id).send()
+            time.sleep(1)
+            self.bot.delete(chat_id=chat_id, message_id=res['result']['message_id']).send()
+            self.bot.more([self.bot.delete(chat_id=chat_id, message_id=message_id),
+                           self.bot.delete(chat_id=chat_id, message_id=res['result']['message_id'])])
+        else:
+            res = self.bot.msg('ERROR: ' + result['result'][0]['content'], chat_id=chat_id, reply_to_message_id=message_id).send()
+            #time.sleep(1)
+            #self.bot.delete(chat_id=chat_id, message_id=res['result']['message_id']).send()
+
+    def stop(self):
+        self.is_run = False
+
+
